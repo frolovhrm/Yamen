39ad85da3235e9a85df2a97fa272222d7909ce7b
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-07-20 18:29:16 +0300

    списки файлов сохраняем в файлы

--- yamen.py
+++ yamen.py
@@ -9,31 +9,25 @@ from PIL import Image, ImageDraw
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
 screenshotspath = 'C:\PetScaner\Screenshert'
-listfiles = []
+fulllistfiles = []
 truelistfile = []
 newfiles = 0
-numtruefiles = 0
-bedfiles = []
+bedlistfiles = []
 one_file = ''
 
 
-def readnewfilesifYandex():
+def readnewfilesifYandex():  # Выбирает скрины из указанной папки ести они с яндекса и пишет в список
     newfiles = 0
     for adress, dirs, files in os.walk(screenshotspath):
         for file in files:
             if 'yandex.taximeter' in file:
-                if files not in listfiles:
-                    listfiles.append(file)
+                if files not in fulllistfiles:
+                    fulllistfiles.append(file)
                     newfiles += 1
     print(f'Добавленно {newfiles} новых файлов')
 
 
-def printlistfiles():
-    for file in listfiles:
-        print(file)
-
-
-def readImagetoText(screenshotname):
+def readImagetoText(screenshotname):  # распознает текст в картинке, сохнаняет в строку
     # image = cv2.imread(name)
     image = change_size(Image.open(screenshotname))
 
@@ -42,14 +36,13 @@ def readImagetoText(screenshotname):
     return string2
 
 
-def check_one(path):
-    # filename = f'C:\PetScaner\Screenshert/{path}'
-    filename = 'C:\PetScaner/venv\Yamen/ans.jpg'
+def read_one(path):
+    filename = f'C:\PetScaner\Screenshert/{path}'
     readedtext = readImagetoText(filename)
     print(f'{readedtext}')
 
 
-def b_w(path):
+def b_w(path):  # Переводит скрин в ЧБ формат и сохнаняет его в новую папку меняя имя
     filename = f'C:\PetScaner\Screenshert/{path}'
 
     image = Image.open(filename)
@@ -73,7 +66,8 @@ def b_w(path):
     image.save(filename, "JPEG")
     del draw
 
-def change_size(img):
+
+def change_size(img):  # изменяет размер скрина до оптимального
     height_size = int(float(img.size[1]) / 3)
     width_size = int(float(img.size[0]) / 3)
     new_image = img.resize((width_size, height_size))
@@ -82,33 +76,81 @@ def change_size(img):
     return new_image
 
 
+def save_resulties():  # Записывает в файлы результаты работы, списки обработаных файлов
+    fullfile = open('fulllist.txt', 'w+')
+    for st in fulllistfiles:
+        fullfile.write(st)
+    fullfile.close()
+
+    truefile = open('truelist.txt', 'w')
+    for st in truelistfile:
+        truefile.write(st)
+    truefile.close()
+
+    bedfile = open('bedlist.txt', 'w')
+    for st in bedlistfiles:
+        bedfile.write(st)
+    bedfile.close()
+
+
+def read_old_true_file_list():  # Читает файл с сохраненным списком файлов пригодных для парсинга
+    try:
+        oldfile = open("truelist.txt")
+        readfile = oldfile.read()
+        oldtruefilelist = readfile.split()
+        oldfile.close()
+        return oldtruefilelist
+    except FileNotFoundError:
+        quest = input('Старый список файлов не обнаружен. Cоздать новый truelist.txt? (Y/N) \n')
+        if quest == 'n':
+            print('Тогда пока')
+            quit()
+        else:
+            print('Будет создан новый список')
+
+def read_old_full_list_files():  # Читает файл с сохраненным списком всех скринов
+    try:
+        oldfile = open("fulllist.txt")
+        readfile = oldfile.read()
+        oldfulllistfiles = readfile.split()
+        oldfile.close()
+        return oldfulllistfiles
+    except FileNotFoundError:
+        quest = input('Старый список файлов не обнаружен. Cоздать новый truelist.txt? (Y/N) \n')
+        if quest == 'n':
+            print('Тогда пока')
+            quit()
+        else:
+            print('Будет создан новый список')
+
 
 readtext = ''
 substreng = ('Самозанятый', 'Сегодня', 'За заказы')
 true_flag = False
+bedlist = ''
+oldtruefilelist = []
 
 readnewfilesifYandex()
 
+read_old_true_file_list()
+
 n = 0
-while n <= 10:  # len(listfiles)
-    filename = f'C:\PetScaner\Screenshert/{listfiles[n]}'
+while n <= 50:  # len(listfiles)
+    filename = f'C:\PetScaner\Screenshert/{fulllistfiles[n]}'
     readedtext = readImagetoText(filename)
     n += 1
     for sub in substreng:
         if readedtext.find(sub) != -1:
-            truelistfile.append(readedtext)
-            numtruefiles += 1
+            truelistfile.append(fulllistfiles[n])
             true_flag = True
             break
         else:
             true_flag = False
     if true_flag == False:
-        bedfiles.append(f'{filename} -----> {readedtext}')
+        bedlistfiles.append(fulllistfiles[n])
 
-print(f'Подходящих файлов {numtruefiles}, битых {len(bedfiles)}')
+print(f'Подходящих файлов {len(truelistfile)}, битых {len(bedlistfiles)}')
 
-for i in truelistfile:
-    print(i)
-print()
+save_resulties()
 
 print("--- %s seconds ---" % int(time.time() - start_time))
