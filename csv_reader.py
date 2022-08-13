78f2977d5c6b4e98ad0b9a0fe7dab5bc1fac9884
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-08-14 00:04:16 +0300

    пишем в csv

--- csv_reader.py
+++ csv_reader.py
@@ -2,15 +2,25 @@ import csv
 
 filename = 'test.csv'
 
-with open(filename, newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
 
-results = []
-with open(filename) as File:
-    reader = csv.DictReader(File)
-    for row in reader:
-        results.append(row)
-        print (results)
-        results = []
+
+with open(filename, mode="w", encoding='utf-8') as w_file:
+    names = ["Имя", "Возраст"]
+    file_writer = csv.DictWriter(w_file, delimiter = ",", lineterminator="\r", fieldnames=names)
+    file_writer.writeheader()
+    for i in range(5):
+        file_writer.writerow({"Имя": i, "Возраст": i})
+
+
+# with open(filename, newline='') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print(row)
+#
+# results = []
+# with open(filename) as File:
+#     reader = csv.DictReader(File)
+#     for row in reader:
+#         results.append(row)
+#         print (results)
+#         results = []
