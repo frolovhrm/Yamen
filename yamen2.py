fa83ed7d63121652e313ac67a576171fddbbec3e
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-08-14 15:34:54 +0300

    парсинг списком

--- yamen2.py
+++ yamen2.py
@@ -8,17 +8,22 @@ import cv2
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
-
-fulllistfiles = []
-truelistfile = []
-bedlistfiles = []
-truetextfileZan = []
-falsetextfile = []
-zan = 0
-seg = 0
-zaza = 0
-
 screenshotspath = 'C:\PetScaner\Screenshert'
+fulllistfiles = []
+activ = float(0.0)
+rait = float(0.0)
+grate = 0
+all_profit = float(0.0)
+cash_profit = 0
+cart_profin = 0
+orders = 0
+orders_str = ''
+income = 0
+commission = 0
+mileage = 0
+balance = 0
+
+
 
 
 def readnewfilesifYandex():  # Выбирает скрины из указанной папки ести они с яндекса и пишет в список
@@ -33,13 +38,14 @@ def readnewfilesifYandex():  # Выбирает скрины из указанн
 
 
 def readImagetoText(filename):  # распознает текст в картинке, сохнаняет в строку
-    screenshotname = f'C:\PetScaner\Screenshert/{filename}'
+    screenshotname = f'{screenshotspath}\{filename}'
     image = cv2.imread(screenshotname)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     config = r'--oem 3 --psm 6'
     string = pytesseract.image_to_string(gray, lang='rus', config=config)
-    string2 = " ".join(string.split())
+    string2 = string.split()
     return string2
+
 
 def nameToDate(name):
     date_str = name.split('_')
@@ -51,32 +57,94 @@ def nameToDate(name):
     print('Дата и время:', date_time_obj)
 
 
+def samozan(str_line):
+    position = 0
+    activ = 0.0
+    rait = 0.0
+    grate = 0
+    all_profit = ' --- '
+    cash_profit = ' --- '
+    cart_profin = ' --- '
+    # print(str_line)
+
+    while position < len(str_line):
+
+        if str_line[position] == 'Самозанятый':
+            activ = int(str_line[position + 1])
+            rait = float(str_line[position + 2])
+            if str_line[position + 3] == 'Бронза':
+                grate = 3
+            if str_line[position + 3] == 'Золото':
+                grate = 2
+            if str_line[position + 3] == 'Платина':
+                grate = 1
+
+        if str_line[position] == 'Сегодня':
+            all_profit = str_line[position + 1]
+            cash_profit = str_line[position + 3]
+            cart_profin = str_line[position + 6]
+
+        position +=1
+
+
+
+    return  activ, rait, grate, all_profit, cash_profit, cart_profin
+
 
 readnewfilesifYandex()
 
-
 i = 0
-while i < 2:
-    stringline = readImagetoText(fulllistfiles[i])
+while i < 15:
+    str_line = readImagetoText(fulllistfiles[i])
+
+    print(f'{samozan(str_line)} - {fulllistfiles[i]}')
     i += 1
-    print(i)
-    nameToDate(fulllistfiles[i])
 
-    if 'занятый' in stringline:
-        zan += 1
-        continue
-    if 'За заказы' in stringline:
-        zaza += 1
-        continue
-    if 'Сегодня' in stringline:
-        seg += 1
-        continue
-    else:
-        falsetextfile.append(stringline)
 
-print(f'Самозанятый {zan}, За заказы {zaza}, Сегодня {seg}, Остальные {len(falsetextfile)}')
-for falsetext in falsetextfile:
-    print(falsetext)
+
+
+
+
+
+
+
+
+
 
 
 print("\n--- %s seconds ---" % int(time.time() - start_time))
+
+
+
+
+
+
+# truelistfile = []
+# bedlistfiles = []
+# truetextfileZan = []
+# falsetextfile = []
+# zan = 0
+# seg = 0
+# zaza = 0
+# i = 0
+# while i < 2:
+#     stringline = readImagetoText(fulllistfiles[i])
+#     i += 1
+#     print(i)
+#     nameToDate(fulllistfiles[i])
+#
+#     if 'занятый' in stringline:
+#         zan += 1
+#         continue
+#     if 'За заказы' in stringline:
+#         zaza += 1
+#         continue
+#     if 'Сегодня' in stringline:
+#         seg += 1
+#         continue
+#     else:
+#         falsetextfile.append(stringline)
+#
+# print(f'Самозанятый {zan}, За заказы {zaza}, Сегодня {seg}, Остальные {len(falsetextfile)}')
+# for falsetext in falsetextfile:
+#     print(falsetext)
