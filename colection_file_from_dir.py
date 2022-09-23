a267e891a9671ed5ddfe2b7335fd19da3f4c8ca6
NewJobHunter <48280515+frolovhrm@users.noreply.github.com>
2022-09-23 23:25:06 +0300

    коллектор файлов дописал

--- colection_file_from_dir.py
+++ colection_file_from_dir.py
@@ -1,21 +1,17 @@
 import os
-import shutil
 
-folderpath = "C:/PetScaner/Screenshert2"
-destinationfolder = "C:/PetScaner/Screenshert2/1"
+folderpath = "C:/PetScaner/Screenshert2/"
+destinationfolder = "C:/PetScaner/Screenshert3/"
 count = 0
 
-
-# folderpath = input('Введите путь к папке с новым сканам: ')
+q = input('Сейчас будет просканирован каталог со скринами, все подходящие файлы\n'
+          'будут перемещены в рабочий каталог (n - выход) - ')
+if q == 'n' or q == 'н':
+    exit()
 
 for files in os.walk(folderpath):
-    print(len(files[2]))
     for file in files[2]:
         if 'yandex.taximete' in file:
-            file1 = f'{folderpath}\\{file}'
-            file2 = f'{destinationfolder}\\{file}'
-            shutil.copyfile(file1, file2)
+            os.replace(folderpath + file, destinationfolder + file)
             count += 1
-        print(f'{count} - {file}')
-
-print(f'Найдено {count} новый файлов.')
+print(f'Перемещено {count} файлов.')
